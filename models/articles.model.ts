c43b484a3e05d5087056aa8a716beb9ded59a433
eycy <96989651+eycy@users.noreply.github.com>
2024-02-23 04:16:18 +0000

    Lab7 - Connect to Postgres

---
+++ models/articles.model.ts
@@ -0,0 +1,61 @@
+import * as db from '../helpers/database';
+
+//get a single article by its id
+export const getById = async (id: any) => {
+  let query = "SELECT * FROM articles WHERE ID = ?"
+  let values = [id]
+  let data = await db.run_query(query, values);
+  return data;
+}
+//list all the articles in the database
+export const getAll = async () => {
+  // TODO: use page, limit, order to give pagination
+  let query = "SELECT * FROM articles;"
+  let data = await db.run_query(query, null);
+  return data;
+}
+//create a new article in the database
+export const add = async (article: any) => {
+  let keys = Object.keys(article);
+  let values = Object.values(article);
+  let key = keys.join(',');
+  let param = '';
+  for (let i: number = 0; i < values.length; i++) { param += '?,' }
+  param = param.slice(0, -1);
+  let query = `INSERT INTO articles (${key}) VALUES (${param})`;
+  try {
+    await db.run_insert(query, values);
+    return { status: 201 };
+  } catch (err: any) {
+    return err;
+  }
+}
+
+// update an article in the database
+export const update = async (article: any, id: any) => {
+  let param = Object.keys(article).map((key) => `${key} = ?`).join(', ')
+  let values = Object.values(article);
+  values.push(id);
+  let query = `UPDATE articles set ${param} where ID = ?`;
+  try {
+    await db.run_update(query, values);
+    return { status: 201 };
+  } catch (err: any) {
+    console.log(err);
+    return err;
+  }
+}
+
+
+// delete an article in the database
+// delete from articles where id = 7
+export const deleteById = async (id: any) => {
+  let values = [id];
+  let query = `delete from articles where ID = ?`;
+  try {
+    await db.run_delete(query, values);
+    return { status: 200 };
+  } catch (err: any) {
+    return err;
+  }
+}
--- routes/articles.ts
+++ routes/articles.ts
@@ -1,62 +1,110 @@
-import Router, {RouterContext} from "koa-router";
+import Router, { RouterContext } from "koa-router";
 import bodyParser from "koa-bodyparser";
+import * as model from '../models/articles.model'
 
-const router = new Router({prefix: '/api/v1/articles'});
+const router = new Router({ prefix: '/api/v1/articles' });
 
 const articles = [
- {title:'hello article', fullText:'some text here to fill the body'},
- {title:'another article', fullText:'again here is some text here to fill'},
- {title:'coventry university ', fullText:'some news about coventry university'},
- {title:'smart campus', fullText:'smart campus is coming to IVE'}
+  { title: 'hello article', fullText: 'some text here to fill the body' },
+  { title: 'another article', fullText: 'again here is some text here to fill' },
+  { title: 'coventry university ', fullText: 'some news about coventry university' },
+  { title: 'smart campus', fullText: 'smart campus is coming to IVE' }
 ];
 
 const getAll = async (ctx: RouterContext, next: any) => {
-  ctx.body = articles;
-  await next();
-}
+  //ctx.body = articles;
 
-const getById = async (ctx: RouterContext, next: any) => {
-  let id = +ctx.params.id;  // + converts to integer (javascript)
-  if ((id < articles.length + 1) && (id > 0)) {
-    ctx.body = articles[id-1];
+  let articles = await model.getAll();
+  if (articles.length) {
+    ctx.body = articles;
   } else {
-    ctx.status = 404;
+    ctx.body = {};
   }
   await next();
 }
 
-const createArticle = async (ctx: RouterContext, next: any) => {
-  let {title, fullText} = ctx.request.body;
-  let newArticle = {title:title, fullText:fullText};
-  articles.push(newArticle);
-  ctx.status = 201;
-  ctx.body = newArticle;
+const getById = async (ctx: RouterContext, next: any) => {
+  // let id = +ctx.params.id;  // + converts to integer (javascript)
+  // if ((id < articles.length + 1) && (id > 0)) {
+  //   ctx.body = articles[id - 1];
+  // } else {
+  //   ctx.status = 404;
+  // }
+
+  let id = +ctx.params.id;
+  let articles = await model.getById(id);
+  if (articles.length)
+    ctx.body = articles[0];
+  else
+    ctx.status = 404;
+
   await next();
 }
 
-const updateArticle = async (ctx: RouterContext, next: any) => {  
-  let id = +ctx.params.id;
-  if ((id < articles.length + 1) && (id > 0)) {
-    let {title, fullText} = ctx.request.body;
-    articles[id-1].title = title;
-    articles[id-1].fullText = fullText;
-    ctx.status = 200;
-    ctx.body = articles[id-1];
+const createArticle = async (ctx: RouterContext, next: any) => {
+  // let { title, fullText } = ctx.request.body;
+  // let newArticle = { title: title, fullText: fullText };
+  // articles.push(newArticle);
+  // ctx.status = 201;
+  // ctx.body = newArticle;
+
+  const body = ctx.request.body;
+  let result = await model.add(body);
+  if (result.status == 201) {
+    ctx.status = 201;
+    ctx.body = body;
   } else {
-    ctx.status = 404;
+    ctx.status = 500;
+    ctx.body = { err: "insert data failed" };
+  }
+
+  await next();
+}
+
+const updateArticle = async (ctx: RouterContext, next: any) => {
+  // let id = +ctx.params.id;
+  // if ((id < articles.length + 1) && (id > 0)) {
+  //   let { title, fullText } = ctx.request.body;
+  //   articles[id - 1].title = title;
+  //   articles[id - 1].fullText = fullText;
+  //   ctx.status = 200;
+  //   ctx.body = articles[id - 1];
+  // } else {
+  //   ctx.status = 404;
+  // }
+
+  const body = ctx.request.body;
+  let id = +ctx.params.id;
+  let result = await model.update(body, id);
+  if (result.status == 201) {
+    ctx.status = 201;
+    ctx.body = body;
+  } else {
+    ctx.status = 500;
+    ctx.body = { err: "update data failed" };
   }
   await next();
 }
 
 
 const deleteArticle = async (ctx: RouterContext, next: any) => {
+  // let id = +ctx.params.id;
+  // if ((id < articles.length + 1) && (id > 0)) {
+  //   articles.splice(id - 1, 1);
+  //   ctx.status = 200;
+  //   ctx.body = articles;
+  // } else {
+  //   ctx.status = 404;
+  // }
+
   let id = +ctx.params.id;
-  if ((id < articles.length + 1) && (id > 0)) {
-    articles.splice(id-1, 1);
+  let result = await model.deleteById(id);
+  if (result.status == 200) {
     ctx.status = 200;
-    ctx.body = articles;
+    ctx.body = id;
   } else {
-    ctx.status = 404;
+    ctx.status = 500;
+    ctx.body = { err: "delete data failed" };
   }
   await next();
 }
